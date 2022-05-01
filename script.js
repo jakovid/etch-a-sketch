6f9903887f0b8c84f3293d792e372d25e369c305
jakovid <jacobk.dowdle@gmail.com>
2022-05-01 13:39:04 +0800

    rage deleted everything. this project is terrible.

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
 /* variables */
 
 /* event listeners */
+document.addEventListener('click', colorChange(Event){
+    Event.target.classList.add('black');
+})
+
 
 /* functions */
 
-function createGrid(dimension) {
-    /* reset(); need to reset the grid somehow*/
-    createBoxes(dimension);
-}
-
-function createBoxes(dimension) {
-    for (let i = 0; i < dimension; i++) {
-        let newBox = document.createElement("box");
-        newBox.className = `box`;
-        newBox.innerHTML = `${i+1}`;
-        newBox.addEventListener("click", colorChange);
-        document.getElementById('container').appendChild(newBox)
-    }
-}
-
-function colorChange() {
-    
-}
