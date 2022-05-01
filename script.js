8990a19c170fcee772a7ae8f1d5186262c53bd2c
jakovid <jacobk.dowdle@gmail.com>
2022-05-01 14:26:26 +0800

    able to create a grid of any size. need to figure out how to reset the board.

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 /* variables */
 
+
 /* event listeners */
-document.addEventListener('click', colorChange(Event){
-    Event.target.classList.add('black');
-})
+
 
 
 /* functions */
 
+function setBoard(size) {
+    let board = document.querySelector('.board');
+    let boxes = document.querySelectorAll('div');
+    /* this doesnt workkkk boxes.forEach((div) --> div.remove()); */
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    for (i = 0; i < size*size; i++) {
+        let box = document.createElement("div");
+        box.className = 'box';
+        box.innerHTML = `${i+1}`;
+        document.getElementById('board').appendChild(box);
+    }
+}
