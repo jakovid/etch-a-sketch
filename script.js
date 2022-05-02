5708efd7ce3b11f87df2e59d656998898251ad0a
jakovid <jacobk.dowdle@gmail.com>
2022-05-02 10:41:53 +0800

    added the ability to change the background color during mouseover event

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 
 /* functions */
 
-function setBoard(size) {
+function makeBoard(size) {
     let board = document.querySelector(".board");
     let boxes = document.querySelectorAll(".box");
     boxes.forEach((div) => div.remove());
@@ -16,6 +16,17 @@ function setBoard(size) {
         let box = document.createElement("div");
         box.className = 'box';
         box.innerHTML = `${i+1}`;
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = "black";
+        });
         document.getElementById('board').appendChild(box);
     }
 }
+
+function setBoard(size) {
+    if(size >= 2 && size <= 100) {
+        makeBoard(size);
+    } else {
+        console.log("size must be between 2 and 100");
+    }
+};
